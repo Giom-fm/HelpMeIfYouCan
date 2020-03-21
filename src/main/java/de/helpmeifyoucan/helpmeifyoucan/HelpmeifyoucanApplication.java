cac0fc99f051ec422535bec2be09f15fde289e36
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2020-03-21 21:38:06 +0100

    remove dangling debug code

--- src/main/java/de/helpmeifyoucan/helpmeifyoucan/HelpmeifyoucanApplication.java
+++ src/main/java/de/helpmeifyoucan/helpmeifyoucan/HelpmeifyoucanApplication.java
@@ -3,17 +3,11 @@ package de.helpmeifyoucan.helpmeifyoucan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-import de.helpmeifyoucan.helpmeifyoucan.utils.Database;
-
 @SpringBootApplication
 public class HelpmeifyoucanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpmeifyoucanApplication.class, args);
-		var test = Database.getDatabase();
-		System.out.println(test.getName());
 	}
 
 }
