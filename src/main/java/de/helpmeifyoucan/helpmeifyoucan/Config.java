2063513a5f07adaca50b4d4cf057b11e11d67218
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2020-03-22 15:36:45 +0100

    Fix typo in "DATABASE_NAME"

--- src/main/java/de/helpmeifyoucan/helpmeifyoucan/Config.java
+++ src/main/java/de/helpmeifyoucan/helpmeifyoucan/Config.java
@@ -4,5 +4,5 @@ public final class Config {
     public static final String DATABASE_ADDRESS = "helpme-1rsgf.mongodb.net/test?retryWrites=true&w=majority";
     public static final String DATABASE_USER = "dbUser";
     public static final String DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");
-    public static final String DATABASE_NAME = "helmeifyoucan";
+    public static final String DATABASE_NAME = "helpmeifyoucan";
 }
