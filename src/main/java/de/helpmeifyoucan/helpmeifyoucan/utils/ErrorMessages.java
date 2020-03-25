7f6a08363a6c26824e1cc11db82262b3d664702f
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2020-03-25 21:24:53 +0100

    Add Errorhandling

--- src/main/java/de/helpmeifyoucan/helpmeifyoucan/utils/ErrorMessages.java
+++ src/main/java/de/helpmeifyoucan/helpmeifyoucan/utils/ErrorMessages.java
@@ -4,5 +4,6 @@ public final class ErrorMessages {
 
     public static final String EMAIL_TAKEN = "Email already taken";
     public static final String USER_NOT_FOUND = "User not found";
+    public static final String ADDRESS_NOT_FOUND = "Address not found";
 
 }
