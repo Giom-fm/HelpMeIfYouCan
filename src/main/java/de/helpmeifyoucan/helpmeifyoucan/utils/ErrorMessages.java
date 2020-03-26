31e60a3ea98cbef90b1ae9b9e4f6f77e6ca1b847
Marc <marc.jaeger@hotmail.de>
2020-03-26 20:14:19 +0100

    fixed merge

--- src/main/java/de/helpmeifyoucan/helpmeifyoucan/utils/ErrorMessages.java
+++ src/main/java/de/helpmeifyoucan/helpmeifyoucan/utils/ErrorMessages.java
@@ -2,15 +2,13 @@ package de.helpmeifyoucan.helpmeifyoucan.utils;
 
 public final class ErrorMessages {
 
-    // Email
     public static final String EMAIL_TAKEN = "Email already taken";
-    // User
     public static final String USER_NOT_FOUND = "User not found";
-    // Address
     public static final String ADDRESS_NOT_FOUND = "Address not found";
-    // Password
+
     public static final String PASSWORD_NOT_SET = "Password is needed";
     public static final String PASSWORD_LENGTH = "Password must contain 8-128 characters";
     public static final String PASSWORD_NUMERIC = "Password must contain at least one numeric character";
     public static final String PASSWORD_ALPHABETIC = "Password must contain at least one alphabetic character";
+
 }
