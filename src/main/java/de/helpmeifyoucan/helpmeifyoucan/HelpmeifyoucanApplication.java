7defee84d82b1d897465090e443cdb918fe89d58
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2020-03-28 00:41:32 +0100

    Add Logging and Application Config

--- src/main/java/de/helpmeifyoucan/helpmeifyoucan/HelpmeifyoucanApplication.java
+++ src/main/java/de/helpmeifyoucan/helpmeifyoucan/HelpmeifyoucanApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.zalando.logbook.Logbook;
 
 @SpringBootApplication
 public class HelpmeifyoucanApplication extends SpringBootServletInitializer {
-
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -20,6 +20,11 @@ public class HelpmeifyoucanApplication extends SpringBootServletInitializer {
     @Bean
     public static MongoDatabase setDatabase() {
         return new Database().getDatabase();
+    }
+
+    @Bean
+    public static Logbook logBook() {
+        return Logbook.create();
     }
 
     public static void main(String[] args) {
