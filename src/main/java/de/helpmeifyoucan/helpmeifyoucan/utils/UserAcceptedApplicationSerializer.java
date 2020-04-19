0e98cfe89389f110677a9617f887f7117d63bf63
Marc <marc.jaeger@hotmail.de>
2020-04-19 20:45:36 +0200

    serialization update

--- src/main/java/de/helpmeifyoucan/helpmeifyoucan/utils/UserAcceptedApplicationSerializer.java
+++ src/main/java/de/helpmeifyoucan/helpmeifyoucan/utils/UserAcceptedApplicationSerializer.java
@@ -30,15 +30,15 @@ public class UserAcceptedApplicationSerializer extends StdSerializer<HashMap<Str
         jsonGenerator.writeStartObject();
 
         jsonGenerator.writeFieldName("received");
-        writeApplication(helpModelApplications.get("received"));
+        writeApplicationReceived(helpModelApplications.get("received"));
 
         jsonGenerator.writeFieldName("send");
-        writeApplication(helpModelApplications.get("send"));
+        writeApplicationSend(helpModelApplications.get("send"));
         jsonGenerator.writeEndObject();
 
     }
 
-    private void writeApplication(List<HelpModelApplication> applications) throws IOException {
+    private void writeApplicationSend(List<HelpModelApplication> applications) throws IOException {
         jsonGenerator.writeStartArray();
         applications.forEach(x -> {
             try {
@@ -50,6 +50,28 @@ public class UserAcceptedApplicationSerializer extends StdSerializer<HashMap<Str
                 jsonGenerator.writeStringField("message", x.getMessage());
                 jsonGenerator.writeStringField("phoneNr", x.getTelephoneNr());
                 jsonGenerator.writeStringField("helpModelType", x.getHelpModelType());
+                jsonGenerator.writeEndObject();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        jsonGenerator.writeEndArray();
+
+    }
+
+    private void writeApplicationReceived(List<HelpModelApplication> applications) throws IOException {
+        jsonGenerator.writeStartArray();
+        applications.forEach(x -> {
+            try {
+                jsonGenerator.writeStartObject();
+                jsonGenerator.writeStringField("id", x.getId().toString());
+                jsonGenerator.writeStringField("modelId", x.getModelId().toString());
+                jsonGenerator.writeStringField("name", x.getName());
+                jsonGenerator.writeStringField("lastName", x.getLastName());
+                jsonGenerator.writeStringField("message", x.getMessage());
+                jsonGenerator.writeStringField("phoneNr", x.getTelephoneNr());
+                jsonGenerator.writeStringField("helpModelType", x.getHelpModelType());
+                jsonGenerator.writeBooleanField("read", x.isRead());
                 jsonGenerator.writeEndObject();
             } catch (IOException e) {
                 e.printStackTrace();
